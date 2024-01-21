59bb36187989eac48f6ad681390e8d8213fa8e58
mateituu <116589732+mateituu@users.noreply.github.com>
2024-01-21 15:43:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Valkyria.dev
+ * Copyright (c) 2011 Valkyria.dev
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
